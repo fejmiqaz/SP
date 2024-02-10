6baa497a37a8294ac81847c3395850e3db57b39b
Fejmi <fejmiqaz@gmail.com>
2024-02-10 23:43:44 +0100

    Update 15.cpp

--- Advanced_SP/two/15.cpp
+++ Advanced_SP/two/15.cpp
@@ -1,17 +1,29 @@
 #include<iostream>
 using namespace std;
 
+void recursive(int n){
+    if( n == 0){
+        return ;
+    }
+    recursive(n - 1);
+    cout << n;
+}
+
+void printPattern(int n, int max){
+    if(n > max){
+        return ;
+    }
+    
+    recursive(n);
+    cout << endl;
+    printPattern(n+1,max);
+}
 
 int main(){
     int n;
     cin >> n;
 
-    for(int i = 1; i <= n; i++){
-        for(int j = 1; j <= i; j++){
-            cout << j;
-        }
-        cout << endl;
-    }
+    printPattern(1,n);
 
     return 0;
 }
