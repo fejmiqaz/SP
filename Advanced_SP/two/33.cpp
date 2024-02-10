13f309b42770a5f94a926611cf6b887c6bc08bb6
Fejmi <fejmiqaz@gmail.com>
2024-02-10 22:27:38 +0100

    Add files via upload

---
+++ Advanced_SP/two/33.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+// SHOUTOUT TO MY FRIEND FLAMUR LIMANI FOR HELPING ME ON THIS TASK, HE THE TRUE GOAT
+
+int main(){
+    int n,m, f[100][100];
+    cin >> n >> m;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            cin >> f[i][j];
+        }
+    }
+    int y = 2, x = 2;
+    while(x <= n && x <= m){
+        int sum1 = 0;
+        for(int i = 0; i < x; i++){
+            sum1 += f[0][i];
+        }
+        int k = 1, l = x - 2;
+        for(int i = 1; i < x - 1; i++){
+            sum1 += f[k][l];
+            k++;
+            l--;
+        }
+        for(int i = 0; i < x; i++){
+            sum1 += f[x-1][i];
+        }
+        x++;
+        y++;
+        cout << sum1 << endl;
+    }
+
+    return 0;
+}
