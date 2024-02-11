a8b531f1a95e232aeda00b52218811fea52123d1
Fejmi <fejmiqaz@gmail.com>
2024-02-11 19:44:21 +0100

    Update 35.cpp

--- Advanced_SP/two/35.cpp
+++ Advanced_SP/two/35.cpp
@@ -3,13 +3,13 @@
 using namespace std;
 
 void words(char f[100], char q[100]){
-    int counter = 0;
-    char *ptr = f;
-    while((ptr = strstr(ptr,q)) != nullptr){
+    int counter = 0; // this counter is to calculate how many times the word appears in the string
+    char *ff = f; // copy the f string to a pointer
+    while((ff = strstr(ff,q)) != nullptr){ // check if the word appears inside the string
         counter++;
-        ptr++;
+        ff++;
     }
-    cout << counter << ": " << f << endl;
+    cout << counter << ": " << f << endl; // print out how many times the word appeared alongside with the whole array
 }
 
 int main() {
