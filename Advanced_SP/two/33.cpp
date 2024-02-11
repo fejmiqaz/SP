ca6e94660cfbfae663c70eee3c0cadc8fa2253a1
Fejmi <fejmiqaz@gmail.com>
2024-02-11 19:41:30 +0100

    Update 33.cpp

--- Advanced_SP/two/33.cpp
+++ Advanced_SP/two/33.cpp
@@ -11,22 +11,24 @@ int main(){
             cin >> f[i][j];
         }
     }
-    int x = 2;
+    // ^^^^^ finish inputs
+    
+    int x = 2; // set a starting value for the box inside the matrix, this means rows go <= 2, and cols <= 2
     while(x <= n && x <= m){
         int sum = 0;
         for(int i = 0; i < x; i++){
-            sum += f[0][i];
+            sum += f[0][i]; // this is for the 0th row
         }
         int k = 1, l = x - 2;
         for(int i = 1; i < x - 1; i++){
-            sum += f[k][l];
+            sum += f[k][l]; // this is the anti-diagonal
             k++;
             l--;
         }
         for(int i = 0; i < x; i++){
-            sum += f[x-1][i];
+            sum += f[x-1][i]; // this is the last row
         }
-        x++;
+        x++; // incrementing the box inside the matrix by 1 each time you finish summing the elements, which means rows++, cols++
         cout << sum << endl;
     }
 
