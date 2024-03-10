f5f28ea72e01a6802801e82430d350d160cd7528
Fejmi <fejmiqaz@gmail.com>
2024-03-10 19:30:36 +0100

    Update ex41.cpp

--- 7/ex41.cpp
+++ 7/ex41.cpp
@@ -30,16 +30,3 @@ int main() {
 
     return 0;
 }
-/* for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            if (f[i][j] % 2 == 0) {
-                f[i][j] = i + j;
-            }
-        }
-    }
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            cout << f[i][j] << " ";
-        }
-        cout << endl;
-    } */
