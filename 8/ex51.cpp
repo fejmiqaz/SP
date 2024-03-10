0105f8b08332dba5a58466bcc386cfce68125240
Fejmi <fejmiqaz@gmail.com>
2024-03-10 19:28:11 +0100

    Update ex51.cpp

--- 8/ex51.cpp
+++ 8/ex51.cpp
@@ -4,16 +4,6 @@ using namespace std;
 /* Write a recursive function that prints out the number of occurrences of a number k in an array of N elements
  * (N is read from SI, as well as the array). If the array doesn't contain the number k, print 0.*/
 
-/*int searchOccurence(int a[], int x, int y){
-    if(x == 0){
-        return 0;
-    }
-    if(a[0] == y){
-        return 1 + searchOccurence(a+1,x-1,y);
-    }
-    return searchOccurence(a+1,x-1,y);
-}*/
-
 int searchOccurrence(int a[], int x, int y){
   if(x == 0){
       return 0;
